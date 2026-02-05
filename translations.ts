109817f151022d54d5fc6432c3bd478fbbe9df44
wenhaochung1404-ship-it <wenhaochung1404@gmail.com>
2026-02-05 15:59:09 +0800

    Update translations.ts

--- translations.ts
+++ translations.ts
@@ -351,7 +351,7 @@ export const translations: Record<Language, Record<string, string>> = {
         "logout": "Log Keluar",
         "home": "Rumah",
         "photo_gallery": "GAMBAR",
-        "coming_soon": "DEKA DATA!!!!",
+        "coming_soon": "DATAI IS COME!!!!",
         "request_help": "Minta Tulung",
         "offer_help": "Meri Tulung",
         "points_shop": "Kedai Mata",
